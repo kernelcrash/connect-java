ab58e1563c5402905800acbdd0960c41a0b7ec85
Kernelcrash <kernelcrash@kernelcrash.com>
2019-08-20 15:03:36 +1200

    Factor out copySnapshot

---
+++ maven-plugins/sanitize-rds-plugin/src/main/groovy/cd/connect/aws/rds/copyDbSnapshotMojo.groovy
@@ -0,0 +1,44 @@
+package cd.connect.aws.rds
+
+import groovy.transform.CompileStatic
+import org.apache.maven.plugin.AbstractMojo
+import org.apache.maven.plugin.MojoExecutionException
+import org.apache.maven.plugin.MojoFailureException
+import org.apache.maven.plugins.annotations.LifecyclePhase
+import org.apache.maven.plugins.annotations.Mojo
+import org.apache.maven.plugins.annotations.Parameter
+import org.apache.maven.plugins.annotations.ResolutionScope
+
+/**
+ * Created by Richard Vowles on 24/07/18.
+ */
+@Mojo(name = "rds-copydbsnapshot",
+	defaultPhase = LifecyclePhase.NONE,
+	requiresProject = false,
+	requiresDependencyResolution = ResolutionScope.NONE, threadSafe = true)
+@CompileStatic
+class CopyDbSnapshotMojo extends AbstractMojo {
+	@Parameter(property = "rds-dbsnapshot.snapshotCopySourceName")
+	String snapshotCopySourceName
+	@Parameter(property = "rds-dbsnapshot.snapshotCopyDestinationName")
+	String snapshotCopyDestinationName
+	@Parameter(property = "rds-dbsnapshot.snapshotCopyDestinationRegion")
+	String snapshotCopyDestinationRegion
+
+	@Parameter(property = "rds-dbsnapshot.skip-rds") // this makes no sense as a property, but we can't is it in an execution if we don't have this
+	boolean skip
+
+	protected RdsClone rdsClone;
+
+	@Override
+	void execute() throws MojoExecutionException, MojoFailureException {
+		if (skip) {
+			return
+		}
+
+		rdsClone = new RdsClone()
+		rdsClone.initialize(awsProfile)
+
+		getLog().info("Copy DB Snapshot from ${snapshotCopySourceName} to ${snapshotCopyDestinationRegion} : ${snapshotCopyDestinationName}")
+	}
+}
